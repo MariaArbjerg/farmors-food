637ba29b9afdbc280a2f3fabe41bbb430ab3e144
MariaArbjerg <mararb01@iba.dk>
2025-12-19 14:21:20 +0100

    Tilføjet søgefunktion i desktop! (Men nu forsvandt den i mobil!!!)

--- js/burger.js
+++ js/burger.js
@@ -4,3 +4,20 @@ const navMenu = document.querySelector("nav ul");
 burger.addEventListener("click", () => {
   navMenu.classList.toggle("nav-open");
 });
+
+
+const searchToggle = document.querySelector(".search-toggle");
+const searchWrapper = document.querySelector(".search-wrapper");
+const searchInput = document.querySelector(".search-input");
+
+searchToggle.addEventListener("click", () => {
+  searchWrapper.classList.toggle("active");
+  searchInput.focus();
+});
+
+// Luk hvis man klikker udenfor
+document.addEventListener("click", (e) => {
+  if (!searchWrapper.contains(e.target)) {
+    searchWrapper.classList.remove("active");
+  }
+});
